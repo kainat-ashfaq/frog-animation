4fa936bc682355484ca1e9923a4c86a8c09cf567
kainat-ashfaq <kainat.ashfaq135@gmail.com>
2024-07-08 21:54:33 +0500

    frog

---
+++ src/script.js
@@ -0,0 +1,47 @@
+
+
+
+// // var num = [1,2,3,4,5,,6,7,8,9,10]
+
+// // var buttons = document.querySelectorAll("#btn")
+
+// // buttons.forEach(function(button) {
+// //     button.addEventListener("click", function() {
+// // for (let i = 0; i <num.length; i++) {
+// //   if (buttons.innerHTML == num[i]) {
+// //     alert(`you click ${num[i]} btn`)
+// //     break
+// //   }else{
+// //     alert(`worng`)
+// //     break
+// //   }
+    
+// // }
+
+        
+// //     });
+// // });
+
+// var btn_1 = document.getElementById("btn-1")
+// var btn_2 = document.getElementById("btn-2");
+
+// var btn_3 = document.getElementById("btn-3");
+// var frog = document.getElementById("frog")
+// btn_1.addEventListener("click",function () {
+//     frog.classList.remove("frog-move");
+//     // frog.classList.add("frog-reversed");
+//     frog.classList.add("frog-stop")
+
+// })
+
+// btn_2.addEventListener("click", function () {
+//   frog.classList.remove("frog-move");
+// //   frog.classList.add("frog-reversed");
+//   frog.classList.add("frog-stop")
+// });
+
+// btn_3.addEventListener("click", function () {
+//   frog.classList.remove("frog-move");
+// //   frog.classList.add("frog-reversed");
+//   frog.classList.add("frog-stop")
+// });
